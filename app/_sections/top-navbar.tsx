e80f1c36b51acbec7c177d4d2b93941ba8bfdb99
jayrick-test <jirk24cay0988@gmail.com>
2025-08-11 11:17:58 +0800

    Adjust top navbar

--- app/_sections/top-navbar.tsx
+++ app/_sections/top-navbar.tsx
@@ -11,48 +11,50 @@ const TopNavbar = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 w-full">
-      <div className="flex items-center justify-between shadow-lg py-12 px-16">
-        <nav className="flex items-center justify-center gap-3.5">
-          <Link
-            href="/"
-            className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
-              isActive("/")
-                ? "text-green-300 border-b border-green-300"
-                : "text-slate-500"
-            }`}
-          >
-            Home
+      <div className="shadow-lg py-12 px-10">
+        <div className="container mx-auto flex items-center justify-between">
+          <nav className="flex items-center justify-center gap-4">
+            <Link
+              href="/"
+              className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
+                isActive("/")
+                  ? "text-green-300 border-b border-green-300"
+                  : "text-slate-500"
+              }`}
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
+                isActive("/about")
+                  ? "text-green-300 border-b border-green-300"
+                  : "text-slate-500"
+              }`}
+            >
+              About
+            </Link>
+            <Link
+              href="/contact"
+              className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
+                isActive("/contact")
+                  ? "text-green-300 border-b border-green-300"
+                  : "text-slate-500"
+              }`}
+            >
+              Contact
+            </Link>
+          </nav>
+          <Link href="/" className="inline-block text-3xl font-bold">
+            <span>Jay</span>
+            <span className="text-green-300">rick</span>
+            <span className="text-blue-400">.</span>
           </Link>
-          <Link
-            href="/about"
-            className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
-              isActive("/about")
-                ? "text-green-300 border-b border-green-300"
-                : "text-slate-500"
-            }`}
-          >
-            About
-          </Link>
-          <Link
-            href="/contact"
-            className={`inline-block hover:text-green-300 hover:border-b hover:border-green-300 transition-all duration-75 ease-in-out ${
-              isActive("/contact")
-                ? "text-green-300 border-b border-green-300"
-                : "text-slate-500"
-            }`}
-          >
-            Contact
-          </Link>
-        </nav>
-        <Link href="/" className="inline-block text-3xl font-bold">
-          <span>Jay</span>
-          <span className="text-green-300">rick</span>
-          <span className="text-blue-400">.</span>
-        </Link>
-        <div>
-          <button className="bg-green-300 text-white rounded p-2 hover:bg-green-400 cursor-pointer transition-colors duration-200">
-            Download CV
-          </button>
+          <div>
+            <button className="bg-green-300 text-white rounded p-2 hover:bg-green-400 cursor-pointer transition-colors duration-200">
+              Download CV
+            </button>
+          </div>
         </div>
       </div>
     </header>
