64cab4835db8242ca6ca36d55ced2f39be7949fb
jayrick-test <jirk24cay0988@gmail.com>
2025-08-15 11:49:48 +0800

    update fixed on cv hooks

--- hooks/cv-hooks.ts
+++ hooks/cv-hooks.ts
@@ -1,13 +1,14 @@
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 
 export const useCV = () => {
-  const myCVPath = useMemo(() => {
-    let cvPath = "http://localhost:3000";
+  const [cvPath, setCVPath] = useState<string>(
+    `${process.env.LOCAL_BASE_DOMAIN_URL || "http://localhost:3000"}/cv/cv.pdf`
+  );
 
-    if (typeof window !== "undefined") cvPath = window.location.origin;
-
-    return `${cvPath}/cv/cv.pdf`;
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      setCVPath(`${window.location.origin}/cv/cv.pdf`);
   }, []);
 
-  return myCVPath;
+  return cvPath;
 };
